e04f7df37d1b5fb5cb5425cab9167acab0d9e939
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-02-04 23:43:32 +0530

    changes

--- middlewares/userProtect.js
+++ middlewares/userProtect.js
@@ -4,7 +4,6 @@ const User = require("../models/userModel");
 const protectUser = async (req, res, next, role) => {
   try {
     const _token = req.cookies._token;
-
     if (!_token) {
       return res.status(401).json({ message: "Token does not exists" });
     }
@@ -17,14 +16,26 @@ const protectUser = async (req, res, next, role) => {
     if (role === undefined) {
       // Open for all 3
       req.user = root_user;
+
+      // Set the secure cookie here
+      res.cookie('_token', _token, { secure: true, sameSite: 'None' });
+
       next();
     } else if (role && root_user.role === role) {
       // Open for specific
       req.user = root_user;
+
+      // Set the secure cookie here
+      res.cookie('_token', _token, { secure: true, sameSite: 'None' });
+
       next();
     }else if(role=="Both" && (root_user.role === "Rector" || root_user.role === "Accountant")){
       // Open for both Rector and Accountant
       req.user = root_user;
+
+      // Set the secure cookie here
+      res.cookie('_token', _token, { secure: true, sameSite: 'None' });
+      
       next();
     } 
     else {
