009c92ba53a0fed2495be84f94b032523323dd40
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-02-04 22:57:19 +0530

    changes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,32 +1,32 @@
-const instance = require("../config/server");
+// const instance = require("../config/server");
 
-const checkout = async (req, res) => {
-  const amount = req.body.amount;
-  // console.log(amount);
+// const checkout = async (req, res) => {
+//   const amount = req.body.amount;
+//   // console.log(amount);
 
-  const options = {
-    amount: Number(amount * 100), // amount in the smallest currency unit
-    currency: "INR",
-  };
-  const order = await instance.orders.create(options);
+//   const options = {
+//     amount: Number(amount * 100), // amount in the smallest currency unit
+//     currency: "INR",
+//   };
+//   const order = await instance.orders.create(options);
 
-  // console.log(order);
+//   // console.log(order);
 
-  return res.status(200).json({
-    success: true,
-    order,
-  });
-};
+//   return res.status(200).json({
+//     success: true,
+//     order,
+//   });
+// };
 
-const paymentVerification = async (req, res) => {
-  console.log(req.body);
+// const paymentVerification = async (req, res) => {
+//   console.log(req.body);
 
-  res.status(200).json({
-    success: true,
-  });
-};
+//   res.status(200).json({
+//     success: true,
+//   });
+// };
 
-module.exports = {
-  checkout,
-  paymentVerification,
-};
+// module.exports = {
+//   checkout,
+//   paymentVerification,
+// };
